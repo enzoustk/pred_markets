25ad2136ace8df2286fe22d429a93676fae532e7
Enzo Araújo <enzo@strikerinvest.com>
2025-10-29 18:32:52 -0300

    Update config.py

--- api/config.py
+++ api/config.py
@@ -3,16 +3,10 @@ Configurações e constantes para chamadas às APIs da polymarket
 """
 
 URLS = {
-    "TRADES_API": "https://data-api.polymarket.com/trades",
-    "ACTIVITY_API": "https://data-api.polymarket.com/activity",
-}
-
-PARAMS = {
-    "LIMIT": 500,
-    "TIMEOUT": 20,
-    "SLEEP": 0.25,
-    "MAX_PAGES": 20_000,
-    "MAX_RECORDS": 300_000
+    "TRADES": "https://data-api.polymarket.com/trades",
+    "ACTIVITY": "https://data-api.polymarket.com/activity",
+    "CLOSED_POSITIONS": "https://data-api.polymarket.com/closed-positions",
+    "ACTIVE_POSITIONS": "https://data-api.polymarket.com/positions",
 }
 
 ACTION_TYPES = [
