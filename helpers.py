352d2e62ce702a980abf7e20ac6a599847c6cabe
Enzo Araújo <enzo@strikerinvest.com>
2025-11-02 20:24:00 -0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,3 +1,16 @@
+import ast
+
 def safe_divide(num, den):
     try: return num/den
     except: return None
+
+def to_list(x):
+    if isinstance(x, list):
+        return x
+    if isinstance(x, str):
+        try:
+            v = ast.literal_eval(x)
+            return v if isinstance(v, list) else []
+        except:
+            return []
+    return []
