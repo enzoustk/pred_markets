0905a830d1c05962cf6d4041fb6d3f5a76313e19
Enzo Araújo <enzo@strikerinvest.com>
2025-11-14 01:13:46 -0300

    new api params required

--- api/config.py
+++ api/config.py
@@ -7,7 +7,10 @@ URLS = {
     "ACTIVITY": "https://data-api.polymarket.com/activity",
     "CLOSED_POSITIONS": "https://data-api.polymarket.com/closed-positions",
     "ACTIVE_POSITIONS": "https://data-api.polymarket.com/positions",
-    "MARKET": "https://gamma-api.polymarket.com/markets"
+    "MARKET": "https://gamma-api.polymarket.com/markets",
+    "POSITIONS_SUBGRAPH": "https://api.goldsky.com/api/public/project_cl6mb8i9h0003e201j6li0diw/subgraphs/positions-subgraph/0.0.7/gn",
+    "TOTAL_TRADES": "https://data-api.polymarket.com/traded",
+    "CLOB": "https://clob.polymarket.com"
 }
 
 ACTION_TYPES = [
@@ -18,3 +21,52 @@ ACTION_TYPES = [
     "REWARD",
     "CONVERSION"
     ]
+
+QUERYS = {
+    'CLOSED': """
+        query GetUserPositions($userAddress: String!, $first: Int!, $skip: Int!) {
+          userBalances(
+            where: { user: $userAddress, balance: "0" }
+            first: $first
+            skip: $skip
+            orderBy: balance
+            orderDirection: desc
+          ) {
+            id
+            user
+            balance
+            asset {
+              id
+              condition {
+                id
+              }
+              outcomeIndex
+            }
+          }
+        }
+        """,
+        
+    'ACTIVE':"""
+        query GetUserPositions($userAddress: String!, $first: Int!, $skip: Int!) {
+          userBalances(
+            where: { user: $userAddress }
+            first: $first
+            skip: $skip
+            orderBy: balance
+            orderDirection: desc
+          ) {
+            id
+            user
+            balance
+            asset {
+              id
+              condition {
+                id
+              }
+              outcomeIndex
+            }
+          }
+        }
+        """,
+
+}
