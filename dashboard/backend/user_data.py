fbdad38a3a1697afc3484c689d1586d76393db5f
Enzo Araújo <enzo@strikerinvest.com>
2025-11-22 12:02:19 -0300

    Improve formatting

--- dashboard/backend/user_data.py
+++ dashboard/backend/user_data.py
@@ -3,15 +3,17 @@ import pandas as pd
 import streamlit as st
 from api.fetch import fetch_total_trades
 from api.fetch_subgraph import fetch_pnl_data
+from dashboard.ui.formatting import center_text
 
 
-def select_user():
+def select_user(
+    ) -> str:
+    # Retora um user válido    
     st.subheader("Select User")
 
     # campo de input do chat
     user_address = st.chat_input("Type user address")
-    st.divider()
-    st.markdown('Hint: 0x507e52ef684ca2dd91f90a9d26d149dd3288beae')
+    center_text('Hint: 0x507e52ef684ca2dd91f90a9d26d149dd3288beae', size=12)
     
     # se o usuário enviou algo
     if user_address:
@@ -27,8 +29,12 @@ def select_user():
             # guardar no session_state se quiser
             st.session_state["selected_wallet"] = user_address
             return user_address
-            
-def get_trades(user_address: str) -> pd.DataFrame:
+
+       
+def get_trades(
+    user_address: str
+    ) -> pd.DataFrame:
+    # Wrapper para puxar todas as Posições do User
     with st.spinner("Fetching trades..."):
         return fetch_pnl_data(user_address)
 
